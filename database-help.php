cdb6c1ba9d096b0ceb1ae71b2256346613636bff
Owen <owen@Owens-MacBook-Pro.local>
2020-03-08 18:52:46 +0000

    Update DB Help

--- database-help.php
+++ database-help.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<head>     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <!--Get the filename and format it for the title of the page-->
     <title>
@@ -11,30 +12,21 @@
 </head>
 
 <body>
+     <?php require_once('navbar.php'); ?>
+    <main>
+        <h2>Database Diagram</h2>
+    <img src="lab20-db-schema.png">
 
-        <?php require_once('navbar.php'); ?>
-  <main>
+
+        <h2>SQL Dump
+        </h2>
         <?php
-$filename = $_GET['filename'] ;
+highlight_file('DB_LIBRARY_Export_books_members_authors_publsihers_loans_tables.sql');
 
-
-if($filename!='db-connect.php'){
-?>
-
-            <h2>Show Code:
-                <?php echo $filename  ?>
-            </h2>
-            <?php
-highlight_file($filename );
-
-}
-else{
-    echo '<h2>Sorry: Cant show code for file - too sensitive!</h2>';
-}
 ?>
 
 
-                <!--end wrapper div-->
+        <!--end wrapper div-->
     </main>
 </body>
 
