5f8487d8d7fc1b0574bd00a3a54a41ad5053b082
Owen <owen@Owens-MacBook-Pro.local>
2020-03-09 20:33:05 +0000

    v2

--- db-connect.php
+++ db-connect.php
@@ -1,6 +1,6 @@
 <?php
-$con = mysqli_connect("localhost", "root", "root", "DB_LIBRARY");
-//$con = mysqli_connect("localhost", "joe", "0seO6u&2", "DB_LIBRARY_2020");
+//$con = mysqli_connect("localhost", "root", "root", "DB_LIBRARY");
+$con = mysqli_connect("localhost", "joe", "0seO6u&2", "DB_LIBRARY_2020");
 
 if (!$con) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
