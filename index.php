9140a49895bc5fffe216dc3cd27b5a242e7fd8e3
Owen <owen@Owens-MacBook-Pro.local>
2020-03-09 22:18:11 +0000

    v3

--- index.php
+++ index.php
@@ -5,8 +5,33 @@
 </head>
 
 <body>
+    <?php require 'db-connect.php'?>
     <?php require 'navbar.php'?>
     <main>
+        <?php
+        $sql='SELECT COUNT(copy_id) AS copyCount FROM t_copies';
+
+        $result=mysqli_query($con,$sql);
+
+        $rowCopies = mysqli_fetch_array($result);
+
+        $sql="SELECT COUNT(*) AS memberCount FROM t_members";
+
+        $result=mysqli_query($con,$sql);
+
+        $rowMembers = mysqli_fetch_array($result);
+
+    ?>
+
+
+        <p>
+            Totals number of books:
+            <?php
+    echo $rowCopies['copyCount'];?><br> Totals number of members:
+            <?php
+    echo $rowMembers['memberCount'];?>
+
+        </p>
 
     </main>
 </body>
